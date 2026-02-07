844dc67b48e181dc474d2bc7885e10876c6649da
alex <alex@pdp7.net>
2026-02-07 21:52:17 +0100

    Add converter

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # `gemini-to-web`
 
 ```
+rm -rf target
+gemini-to-web example/ target/
+```
+
+```
 uv run python -m doctest -v src/gemini_to_web/parser.py
 ```
--- src/gemini_to_web/__init__.py
+++ src/gemini_to_web/__init__.py
@@ -1,2 +1,25 @@
-def main() -> None:
-    print("Hello from gemini-to-web!")
+import argparse
+import pathlib
+import shutil
+
+import htmlgenerator
+
+from gemini_to_web import html
+
+def converter():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("source", type=pathlib.Path)
+    parser.add_argument("target", type=pathlib.Path)
+    args = parser.parse_args()
+
+    shutil.copytree(args.source, args.target)
+    for gmi in args.target.glob("**/*.gmi"):
+        html_path = gmi.with_suffix(".html")
+        html_path.write_text(
+            html.pretty(
+                htmlgenerator.render(
+                    html.to_html(gmi.read_text()),
+                    {}
+                )
+            )
+        )
